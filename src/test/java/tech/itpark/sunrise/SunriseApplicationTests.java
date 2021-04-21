2ff99aa8a0719b4605804432e13376985bbecc7b
Anastasiia787 <nastenaprokopeva787@gmail.com>
2021-04-21 10:05:59 +0300

    initial commit

---
+++ src/test/java/tech/itpark/sunrise/SunriseApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.itpark.sunrise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunriseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
